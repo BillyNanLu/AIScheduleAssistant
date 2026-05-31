a76131e39b4a5c59f1f85625c42c871448041e68
BillyNan_Lu <lunan96789@gmail.com>
2026-05-31 15:26:07 +0800

    fix(backend:planning): modify schedule pojo adding @JsonFormat

--- backend/src/main/java/com/nan19studio/pojo/Schedule.java
+++ backend/src/main/java/com/nan19studio/pojo/Schedule.java
@@ -1,5 +1,6 @@
 package com.nan19studio.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -15,15 +16,20 @@ public class Schedule {
 
     private String description;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
 
     private Integer status;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reminderTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 }
